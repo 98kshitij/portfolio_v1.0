4e078b1dd7e5cd9b17546f0394872130efc99878
98kshitij <kmr.kshitij@gmail.com>
2024-09-20 09:05:48 +0530

    ssd6d

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 const terminalContent = document.getElementById('terminal-content');
 const commandInput = document.getElementById('command-input');
 
-// Fake terminal commands
+// Terminal commands
 const commands = {
-    help: 'Available commands: about, skills, projects, contact, clear',
-    about: 'Hi, I\'m Kshitij, a Software Developer with experience in HTML, CSS, JavaScript, Java and Python.',
-    skills: 'Skills: HTML, CSS, JavaScript, Java, Spring, SpringBoot, and AWS',
-    projects: 'Projects:\n1. Portfolio Website',
-    contact: 'You can reach me via email at kmr.kshitij@gmail.com',
+    help: 'Available commands: about, skills, projects, contact, linkedin, github, email, clear',
+    about: 'Hi, I\'m Kshitij, a passionate web developer with experience in HTML, CSS, and JavaScript.',
+    skills: 'Skills: HTML, CSS, JavaScript, React, Node.js',
+    projects: `
+1. Portfolio Website - A personal portfolio to showcase my projects.`,
+    contact: 'You can reach me via email or check out my profiles on LinkedIn and GitHub.',
+    linkedin: 'LinkedIn profile: https://www.linkedin.com/in/98kshitij',
+    github: 'GitHub profile: https://github.com/98kshitij',
+    email: 'Email: kmr.kshitij@gmail.com',
     clear: ''
 };
 
-// Handle the command input
+// Command input handler
 commandInput.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         const input = commandInput.value.trim().toLowerCase();
-        
-        // Create a new line with the typed command
-        terminalContent.innerHTML += `<div><span class="prompt">[portfolio@kshitij]$</span> ${input}</div>`;
 
-        // Check if the command exists and display the result
+        // Print typed command
+        terminalContent.innerHTML += `<div><span class="prompt">[kshitij@portfolio]$</span> ${input}</div>`;
+
+        // Check if command exists and display result
         if (commands[input]) {
             if (input === 'clear') {
                 terminalContent.innerHTML = ''; // Clear terminal content
@@ -29,11 +33,11 @@ commandInput.addEventListener('keydown', function(event) {
         } else {
             terminalContent.innerHTML += `<div>Command not found: ${input}</div>`;
         }
-        
+
         // Scroll to the bottom
         terminalContent.scrollTop = terminalContent.scrollHeight;
-        
-        // Clear the input field
+
+        // Clear input field
         commandInput.value = '';
     }
 });
